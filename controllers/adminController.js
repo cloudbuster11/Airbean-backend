233129fd6646420365367a54b54a8cda9bf5ba2b
Joakim Jönsson <jockejockejockejocke@gmail.com>
2023-03-09 09:28:33 +0100

    Created an apiFeatures utils that contains reusable features.

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,67 +1,15 @@
 const models = require('../models');
+const Order = require('../models/orderGuestModel');
 const OrderUser = models.orderUser;
 const Product = models.product;
 
-// exports.getAllOrders = async (req, res) => {
-//   try {
-//     const allOrders = await OrderUser.find();
-
-//     res.status(200).json({
-//       status: 'success',
-//       data: {
-//         allOrders,
-//       },
-//     });
-//   } catch (err) {
-//     res.status(404).json({
-//       status: 'fail',
-//       message: err,
-//     });
-//   }
-// };
+const utils = require('../utils/');
+const apiFeatures = utils.apiFeatures;
 
 exports.getAllOrders = async (req, res) => {
   try {
-    // Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // Advanced filter
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
-
-    let query = OrderUser.find(JSON.parse(queryStr));
-
-    //  Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    // page=3&limit=10, 1-10, page 1, 11-20, page 2, 21-30, page 3
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numOrders = await OrderUser.countDocuments();
-      if (skip >= numOrders) throw new Error('This page does not exist');
-    }
-
-    const allOrders = await query;
+    const features = new apiFeatures(OrderUser.find(), req.query).filter().sort().limitFields().paginate();
+    const allOrders = await features.query;
 
     res.status(200).json({
       status: 'success',
--- utils/index.js
+++ utils/index.js
@@ -2,6 +2,7 @@ const utils = {};
 
 utils.calculateNewEta = require('./eta');
 utils.connectToDb = require('./databaseConnector');
+utils.apiFeatures = require('./apiFeatures');
 // utils.initializeRoles = require('./initializeRoles');
 
 module.exports = utils;
